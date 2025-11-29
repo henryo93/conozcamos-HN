80647c40ec289f2567c96691ec18e75547b915f2
henryo93 <henryohara93@gmail.com>
2025-11-28 20:44:33 -0600

    Avance proyecto

---
+++ conozcamos_honduras.sql
@@ -0,0 +1,245 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: conozcamos_honduras
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `dificultad`
+--
+
+DROP TABLE IF EXISTS `dificultad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dificultad` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dificultad`
+--
+
+LOCK TABLES `dificultad` WRITE;
+/*!40000 ALTER TABLE `dificultad` DISABLE KEYS */;
+INSERT INTO `dificultad` VALUES (1,'Basico'),(2,'Experto');
+/*!40000 ALTER TABLE `dificultad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modalidad`
+--
+
+DROP TABLE IF EXISTS `modalidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modalidad` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modalidad`
+--
+
+LOCK TABLES `modalidad` WRITE;
+/*!40000 ALTER TABLE `modalidad` DISABLE KEYS */;
+INSERT INTO `modalidad` VALUES (1,'Verdadero O Falso'),(2,'Opcion Multiple');
+/*!40000 ALTER TABLE `modalidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `orderIdentifier` varchar(100) DEFAULT NULL,
+  `orderLineNumber` varchar(100) DEFAULT NULL,
+  `orderType` varchar(100) DEFAULT NULL,
+  `productpartNumber` varchar(100) DEFAULT NULL,
+  `shipFromInstructionLocationlocationIdentifier` varchar(100) DEFAULT NULL,
+  `shipToLocationlocationIdentifier` varchar(100) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `createdDate` varchar(100) DEFAULT NULL,
+  `requestedShipDate` varchar(100) DEFAULT NULL,
+  `requestedDeliveryDate` varchar(100) DEFAULT NULL,
+  `plannedShipDate` varchar(100) DEFAULT NULL,
+  `plannedDeliveryDate` varchar(100) DEFAULT NULL,
+  `quantity` varchar(100) DEFAULT NULL,
+  `quantityUnits` varchar(100) DEFAULT NULL,
+  `productValue` varchar(100) DEFAULT NULL,
+  `value` varchar(100) DEFAULT NULL,
+  `valueCurrency` varchar(100) DEFAULT NULL,
+  `shipmentCount` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pregunta`
+--
+
+DROP TABLE IF EXISTS `pregunta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pregunta` (
+  `idPregunta` int NOT NULL AUTO_INCREMENT,
+  `idTrivia` int NOT NULL,
+  `idDificultad` int NOT NULL,
+  `idModalidad` int NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idPregunta`),
+  KEY `idTrivia_idx` (`idTrivia`),
+  CONSTRAINT `fk_preguntaTrivia` FOREIGN KEY (`idTrivia`) REFERENCES `trivia` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pregunta`
+--
+
+LOCK TABLES `pregunta` WRITE;
+/*!40000 ALTER TABLE `pregunta` DISABLE KEYS */;
+INSERT INTO `pregunta` VALUES (1,1,1,1,'DE QUE COLOR ES LA BANDERA_');
+/*!40000 ALTER TABLE `pregunta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ranking`
+--
+
+DROP TABLE IF EXISTS `ranking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ranking` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idTrivia` int NOT NULL,
+  `idDificultad` int NOT NULL,
+  `idModalidad` int NOT NULL,
+  `tiempoInicio` int NOT NULL,
+  `tiempoFin` int NOT NULL,
+  `totalPuntos` int NOT NULL,
+  `idUsuario` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ranking`
+--
+
+LOCK TABLES `ranking` WRITE;
+/*!40000 ALTER TABLE `ranking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ranking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `respuesta`
+--
+
+DROP TABLE IF EXISTS `respuesta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `respuesta` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `idPregunta` varchar(45) DEFAULT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `puntos` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `respuesta`
+--
+
+LOCK TABLES `respuesta` WRITE;
+/*!40000 ALTER TABLE `respuesta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `respuesta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trivia`
+--
+
+DROP TABLE IF EXISTS `trivia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trivia` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trivia`
+--
+
+LOCK TABLES `trivia` WRITE;
+/*!40000 ALTER TABLE `trivia` DISABLE KEYS */;
+INSERT INTO `trivia` VALUES (1,'CULTURA GENERAL '),(2,'ENTRETENIMIENTO GENERAL');
+/*!40000 ALTER TABLE `trivia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `correo` varchar(45) NOT NULL,
+  `apodo` varchar(45) NOT NULL,
+  `contrasenia` varchar(45) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-28 20:42:09
